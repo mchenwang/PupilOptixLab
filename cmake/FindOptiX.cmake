76244ff8e5358e15651b41a17d30b98dcaa8dd97
ChenWang <1401520906@qq.com>
2022-11-19 21:05:10 +0800

    add third party

---
+++ cmake/FindOptiX.cmake
@@ -0,0 +1,20 @@
+if(DEFINED ENV{OptiX_INSTALL_DIR})
+    message(STATUS "Detected the OptiX_INSTALL_DIR env variable (pointing to $ENV{OptiX_INSTALL_DIR}; going to use this for finding optix.h")
+    find_path(OptiX_ROOT_DIR NAMES include/optix.h PATHS $ENV{OptiX_INSTALL_DIR})
+elseif(WIN32)
+    find_path(OptiX_ROOT_DIR
+        NAMES include/optix.h
+        PATHS "C:/ProgramData/NVIDIA Corporation/OptiX SDK 7.5.0"
+    )
+else()
+    find_path(OptiX_ROOT_DIR NAMES include/optix.h)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(OptiX
+    FAIL_MESSAGE "Failed to find OptiX install dir. Please instal OptiX or set OptiX_INSTALL_DIR env variable."
+    REQUIRED_VARS OptiX_ROOT_DIR
+)
+
+add_library(OptiX::OptiX INTERFACE IMPORTED)
+target_include_directories(OptiX::OptiX INTERFACE ${OptiX_ROOT_DIR}/include)
