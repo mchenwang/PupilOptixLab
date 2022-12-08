d0a80795a26a5a52b6832599e22e611bfd5baade
ChenWang <1401520906@qq.com>
2022-12-08 19:32:50 +0800

    add xml object

---
+++ src/scene/xml/object.cpp
@@ -0,0 +1,25 @@
+#include "object.h"
+#include "pugixml.hpp"
+
+namespace scene::xml {
+void GlobalManager::AddGlobalParam(std::string name, std::string value) noexcept {
+    global_params[name] = value;
+}
+
+void GlobalManager::ReplaceDefaultValue(pugi::xml_node *node) noexcept {
+    for (auto attr : node->attributes()) {
+        std::string a_value = attr.value();
+        if (a_value.find('$') == std::string::npos)
+            continue;
+        for (auto &[p_name, p_value] : global_params) {
+            size_t pos = 0;
+            std::string temp_name = "$" + p_name;
+            while ((pos = a_value.find(temp_name, pos)) != std::string::npos) {
+                a_value.replace(pos, temp_name.length(), p_value);
+                pos += p_value.length();
+            }
+        }
+        attr.set_value(a_value.c_str());
+    }
+}
+}// namespace scene::xml
---
+++ src/scene/xml/object.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+
+namespace pugi {
+class xml_node;
+}
+
+namespace scene {
+namespace xml {
+/// @brief xml object's property
+/// support integer, string, float, boolean, rgb, (for transform matrix, scale, rotate, translate)
+struct Property {
+    std::string name;
+    std::string value;
+
+    Property(std::string_view name, std::string_view value) noexcept : name(name), value(value) {}
+};
+
+/// @brief support bsdf, emitter, film, integrator, sensor, shape, texture, and transform
+struct Object {
+    std::string obj_name;
+    std::string var_name;
+    std::string type;
+    std::vector<Property> properties;
+    std::vector<Object *> sub_object;
+
+    Object(std::string_view obj_name, std::string_view type) noexcept : obj_name(obj_name), type(type) {}
+};
+
+struct GlobalManager {
+    std::unique_ptr<Object> obj_root = nullptr;
+    Object *current_obj = nullptr;
+    std::vector<std::unique_ptr<Object>> objects_pool;
+    std::unordered_map<std::string, std::string> global_params;
+    std::unordered_map<std::string, Object *> ref_objects_map;
+
+    void AddGlobalParam(std::string, std::string) noexcept;
+
+    void ReplaceDefaultValue(pugi::xml_node *) noexcept;
+};
+}
+}// namespace scene::xml
