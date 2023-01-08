2eeff83acad8bbf686190c3d23e4fb3e19e9a709
ChenWang <1401520906@qq.com>
2023-01-08 22:43:09 +0800

    update Transform

--- src/scene/shape.h
+++ src/scene/shape.h
@@ -5,6 +5,11 @@
 namespace scene {
 struct Transform {
     float matrix[16]{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
+
+    void Translate(float x, float y, float z) noexcept;
+    // rotation axis is [ux, uy, uz]
+    void Rotate(float ux, float uy, float uz, float angle) noexcept;
+    void Scale(float x, float y, float z) noexcept;
 };
 
 enum class EShapeType {
