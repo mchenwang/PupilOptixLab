4e57c0b28eef136dc71b4a52798d65e25e27f061
ChenWang <1401520906@qq.com>
2023-02-05 22:51:33 +0800

    update data view

--- src/cuda_util/data_view.h
+++ src/cuda_util/data_view.h
@@ -9,13 +9,18 @@ template<typename T>
 class ConstArrayView {
 private:
     CUdeviceptr m_data CONST_STATIC_INIT(0);
+    size_t m_num CONST_STATIC_INIT(0);
 
 public:
     CUDA_HOSTDEVICE ConstArrayView() noexcept {}
 
-    CUDA_HOST void SetData(CUdeviceptr cuda_data) noexcept { m_data = cuda_data; }
+    CUDA_HOST void SetData(CUdeviceptr cuda_data, size_t num) noexcept {
+        m_data = cuda_data;
+        m_num = num;
+    }
 
     CUDA_HOSTDEVICE operator bool() const noexcept { return m_data != 0; }
+    CUDA_HOSTDEVICE const size_t GetNum() const noexcept { return m_num; }
     CUDA_HOSTDEVICE const T &operator[](unsigned int index) const noexcept {
         return *reinterpret_cast<T *>(m_data + index * sizeof(T));
     }
