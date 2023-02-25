5bd4046c8d6ecd95f69c7ba229160e5b498cfd2c
ChenWang <1401520906@qq.com>
2023-02-25 21:26:11 +0800

    update util::Float

--- src/common/type.h
+++ src/common/type.h
@@ -15,8 +15,8 @@ struct Float3 {
         float e[3];
     };
 
-    Float3(float xyz = 0.f) noexcept : x(xyz), y(xyz), z(xyz) {}
-    Float3(float x_, float y_, float z_) noexcept : x(x_), y(y_), z(z_) {}
+    constexpr Float3(float xyz = 0.f) noexcept : x(xyz), y(xyz), z(xyz) {}
+    constexpr Float3(float x_, float y_, float z_) noexcept : x(x_), y(y_), z(z_) {}
 
     inline Float3 operator+(const Float3 &t) const noexcept { return { x + t.x, y + t.y, z + t.z }; }
     inline Float3 operator-(const Float3 &t) const noexcept { return { x - t.x, y - t.y, z - t.z }; }
@@ -44,8 +44,8 @@ struct Float4 {
         float e[4];
     };
 
-    Float4(float xyzw = 0.f) noexcept : x(xyzw), y(xyzw), z(xyzw), w(xyzw) {}
-    Float4(float x_, float y_, float z_, float w_) noexcept : x(x_), y(y_), z(z_), w(w_) {}
+    constexpr Float4(float xyzw = 0.f) noexcept : x(xyzw), y(xyzw), z(xyzw), w(xyzw) {}
+    constexpr Float4(float x_, float y_, float z_, float w_) noexcept : x(x_), y(y_), z(z_), w(w_) {}
 
     inline Float4 operator+(const Float4 &t) const noexcept { return { x + t.x, y + t.y, z + t.z, w + t.w }; }
     inline Float4 operator-(const Float4 &t) const noexcept { return { x - t.x, y - t.y, z - t.z, w - t.w }; }
@@ -75,16 +75,17 @@ struct Mat4 {
         DirectX::XMMATRIX dx_mat;
     };
 
-    Mat4() noexcept : r0(0.f), r1(0.f), r2(0.f), r3(0.f) {}
-    Mat4(float e_[16]) noexcept { std::copy(e_, e_ + 16, e); }
-    Mat4(DirectX::XMMATRIX m) noexcept { dx_mat = m; }
-    Mat4(float m00, float m01, float m02, float m03,
-         float m10, float m11, float m12, float m13,
-         float m20, float m21, float m22, float m23,
-         float m30, float m31, float m32, float m33) noexcept
+    constexpr Mat4() noexcept : r0(0.f), r1(0.f), r2(0.f), r3(0.f) {}
+    constexpr Mat4(float e_[16]) noexcept { std::copy(e_, e_ + 16, e); }
+    constexpr Mat4(float m00, float m01, float m02, float m03,
+                   float m10, float m11, float m12, float m13,
+                   float m20, float m21, float m22, float m23,
+                   float m30, float m31, float m32, float m33) noexcept
         : r0(m00, m01, m02, m03), r1(m10, m11, m12, m13), r2(m20, m21, m22, m23), r3(m30, m31, m32, m33) {}
 
+    Mat4(DirectX::XMMATRIX m) noexcept { dx_mat = m; }
     operator DirectX::XMMATRIX() const noexcept { return dx_mat; }
+
     DirectX::XMMATRIX operator*(const Mat4 &t) const noexcept { return dx_mat * t.dx_mat; }
 
     Mat4 GetInverse() const noexcept {
