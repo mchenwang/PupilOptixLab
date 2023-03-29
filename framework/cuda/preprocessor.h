d50b36c117b9b04615e0aa7b9ded269955c06c05
ChenWang <1401520906@qq.com>
2023-03-29 20:44:36 +0800

    add cuda global macro

--- framework/cuda/preprocessor.h
+++ framework/cuda/preprocessor.h
@@ -4,6 +4,7 @@
 #define CUDA_HOST __host__
 #define CUDA_DEVICE __device__
 #define CUDA_HOSTDEVICE __host__ __device__
+#define CUDA_GLOBAL __global__
 #define CUDA_INLINE __forceinline__
 #define CONST_STATIC_INIT(...)
 #else
@@ -11,6 +12,7 @@
 #define CUDA_HOST
 #define CUDA_DEVICE
 #define CUDA_HOSTDEVICE
+#define CUDA_GLOBAL
 #define CUDA_INLINE inline
 #define CONST_STATIC_INIT(...) = __VA_ARGS__
 #endif
