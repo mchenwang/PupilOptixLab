0932a950e5028424a9152d16c40924fef18c15c4
ChenWang <1401520906@qq.com>
2023-04-04 20:38:15 +0800

    embed ptx in const char

--- cmake/FindOptiX.cmake
+++ cmake/FindOptiX.cmake
@@ -28,21 +28,3 @@ target_include_directories(OptiX INTERFACE ${OptiX_ROOT_DIR}/include)
 enable_language(CUDA)
 find_package(CUDA 11.6 REQUIRED)
 find_package(CUDAToolkit 11.6 REQUIRED)
-
-find_program(BIN2C bin2c
-  DOC "Path to the cuda-sdk bin2c executable.")
-
-# code from https://github.com/ingowald/optix7course/blob/master/common/gdt/cmake/configure_optix.cmake
-macro(cuda_compile_and_embed output_var cuda_file)
-  set(c_var_name ${output_var})
-  cuda_compile_ptx(ptx_files ${cuda_file} OPTIONS --generate-line-info -use_fast_math --keep)
-  list(GET ptx_files 0 ptx_file)
-  set(embedded_file ${ptx_file}_embedded.c)
-  add_custom_command(
-    OUTPUT ${embedded_file}
-    COMMAND ${BIN2C} -c --padd 0 --type char --name ${c_var_name} ${ptx_file} > ${embedded_file}
-    DEPENDS ${ptx_file}
-    COMMENT "compiling (and embedding ptx from) ${cuda_file}"
-  )
-  set(${output_var} ${embedded_file})
-endmacro()
