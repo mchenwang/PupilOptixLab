4c48022aaf21b735dafde916fae8d5e7662fa78f
ChenWang <1401520906@qq.com>
2023-04-08 10:33:38 +0800

    add camera view/proj matrix

--- framework/util/camera.h
+++ framework/util/camera.h
@@ -17,9 +17,12 @@ private:
     Mat4 m_rotate_inv;
 
     bool m_to_world_dirty = true;
+    Mat4 m_to_world;// camera to world
+    Mat4 m_view;    // world to camera
+
     bool m_projection_dirty = true;
-    Mat4 m_to_world;
-    Mat4 m_sample_to_camera;
+    Mat4 m_sample_to_camera;// screen to camera
+    Mat4 m_proj;            // camera to screen
 
 public:
     static float sensitivity;
@@ -32,7 +35,9 @@ public:
     Camera() noexcept = default;
 
     Mat4 GetSampleToCameraMatrix() noexcept;
+    Mat4 GetProjectionMatrix() noexcept;
     Mat4 GetToWorldMatrix() noexcept;
+    Mat4 GetViewMatrix() noexcept;
 
     std::tuple<Float3, Float3, Float3> GetCameraCoordinateSystem() const noexcept;
 
